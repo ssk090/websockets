2a3d73ec8f904a8048a488f1344f5b97fd1cf9be
Shivananda Sai <shivanandasai.38@gmail.com>
2025-12-03 22:03:23 +0530

    Refactor WebSocket message handling to respond to 'ping' and remove price updates

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,10 @@ const wss = new WebSocketServer({ port: 8080 });
 
 wss.on("connection", function (socket) {
   console.log("user connected");
-  setInterval(() => {
-    socket.send("current price" + Math.random());
-  }, 1000);
 
   socket.on("message", (e) => {
-    console.log("message from client", e.toString());
+    if (e.toString() === "ping") {
+      socket.send("pong");
+    }
   });
 });
