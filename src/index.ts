5e9b9362d3da8294ebb93d6779ea3434c4fdefc4
Shivananda Sai <shivanandasai.38@gmail.com>
2025-12-03 21:20:56 +0530

    Implement WebSocket server with connection handling and message broadcasting

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello, WebSockets!");
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", function (socket) {
+  console.log("user connected");
+  setInterval(() => {
+    socket.send("current price" + Math.random());
+  }, 1000);
+
+  socket.on("message", (e) => {
+    console.log("message from client", e.toString());
+  });
+});
